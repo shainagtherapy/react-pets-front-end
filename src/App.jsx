da012b08de68583f29f7c686cf275be23204f3b4
Shaina G <shainagtherapy@gmail.com>
2025-08-14 10:12:19 -0700

    Read Show!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from 'react';
+import * as petServices from './services/petService';
+
+import PetList from './components/PetList/PetList';
+import PetDetail from './components/PetDetail/PetDetail';
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState(null)
+
+  useEffect(() => {
+  const fetchPets = async () => {
+    try {
+      const fetchedPets = await petServices.index();
+    
+      if (fetchedPets.err) {
+        throw new Error(fetchedPets.err);
+      }
+
+      setPets(fetchedPets);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  fetchPets();
+}, []);
+
+  const handleSelect = (pet) => {
+    setSelected(pet)
+  }
+
+  return (
+    <>
+    <PetList pets={pets} handleSelect={handleSelect}/>
+    <PetDetail selected={selected} />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/PetList/PetList.jsx
@@ -0,0 +1,28 @@
+const PetList = (props) => {
+
+
+  return (
+    <div>
+      <h1>Pet List</h1>
+      <div>
+        {!props.pets.length ? (
+          <h2>No Pets Yet!</h2>
+        ) : (
+           <ul>
+          {props.pets.map((pet) => (
+            <li 
+              key={pet._id}
+              style={{ cursor: 'pointer', color: "#646CFF" }}
+              onClick={() => props.handleSelect(pet)}
+            >
+              {pet.name}
+            </li>
+          ))}
+        </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PetList;
